afcc68aac7e426a67dba83204c2ad33419e73b54
Yair <yair.notkovich@gmail.com>
2023-03-19 15:48:54 +0200

    base url 0.0.0.0

--- frontend/src/utils/api/urls.js
+++ frontend/src/utils/api/urls.js
@@ -2,7 +2,7 @@
 // import { REACT_APP_PLACES_API_KEY } from "./.secret"
 
 
-export const BASE_URL = "http://127.0.0.1:8000"
+export const BASE_URL = "http://0.0.0.0:8000"
 
 export const IMAGE_URL = (src_url)=>(BASE_URL+`${src_url}`)
 
